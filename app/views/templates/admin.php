427b09d8e823ae9bf0b23ab7555dd2c7e2a56902
yura <yura@com.ua>
2018-05-10 20:21:15 +0300

    add admin.php

--- app/views/templates/admin.php
+++ app/views/templates/admin.php
@@ -1,10 +1,22 @@
 <!DOCTYPE html>
 <html lang="ru">
-<head>
-	<meta charset="utf-8">
-	<title>Главная</title>
-</head>
-<body>
-	<?php include 'app/views/'.$this->content_view; ?>
+    <head>
+        <meta charset="utf-8">
+    <h1>Админка</h1>
+    <div>
+        <p>Hello <?= $_SESSION['login']; ?></p>
+    </div>
+    <p><a href="/auth/signout">Выйти</a></p>
+    <div>
+        <h2>Меню</h2>
+        <ul>
+            <li><a href="/admin/orders">Orders</a></li>
+            <li><a href="/admin/concerts">Concerts</a></li>
+            <li><a href="/admin/users">Users</a></li>
+        </ul>
+    </div>
+    <section>
+        <?php include_once $content_template_file; ?>
+    </section>
 </body>
 </html>
