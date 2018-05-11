31efa9c9d966f2c9a731bc3b1c621c7e455f3206
alex3fokin <alex3.fokin@gmail.com>
2018-05-11 21:50:00 +0300

    made edit|delete concerts functionality

--- app/models/Model_Concerts.php
+++ app/models/Model_Concerts.php
@@ -13,9 +13,6 @@ class Model_Concerts extends Model {
     
     public function get_all_concerts() {
 	$query = 'select * from '.$this->concerts_table;
-//        $stmt = $dbh->prepare("INSERT INTO REGISTRY (name, value) VALUES (?, ?)");
-//        $stmt->bindParam(1, $name);
-//        $stmt->bindParam(2, $value);
         $stmt = $this->db->prepare($query);
         $stmt->execute();
         if($result = $stmt->fetchAll()) {
@@ -24,20 +21,53 @@ class Model_Concerts extends Model {
         return false;
     }
 
+    public function get_one_concert($id) {
+        $query = 'select * from '.$this->concerts_table.' where id='.$id;
+        $stmt = $this->db->prepare($query);
+        $stmt->execute();
+        if($result = $stmt->fetch()) {
+            return $result;
+        }
+        return false;
+    }
+    
     public function get_all_orders() {
 	
     }
 
-    public function insert_concert($image, $date_time, $cost, $description) {
-	
+    public function insert_concert($image, $date_time, $price, $description) {
+	$query = 'insert into '.$this->concerts_table.' (image, date, price, description) values(:image, :date,:price,:desc)';
+        $stmt = $this->db->prepare($query);
+        $stmt->bindParam(':image', $image);
+        $stmt->bindParam(':date', $date_time);
+        $stmt->bindParam(':price', $price);
+        $stmt->bindParam(':desc', $description);
+        if($stmt->execute()) {
+            return true;
+        }
+        return false;
     }
 
     public function delete_concert($id) {
-	
+	$query = 'delete from '.$this->concerts_table.' where id='.$id;
+        $stmt = $this->db->prepare($query);
+        if($stmt->execute()) {
+            return true;
+        }
+        return false;
     }
 
-    public function update_concert($id, $image, $date_time, $cost, $description) {
-	
+    public function update_concert($id, $date_time, $price, $description) {
+	$query = 'update '.$this->concerts_table.' set date=:date, price=:price, description=:desc where id=:id';
+        $stmt = $this->db->prepare($query);
+        $stmt->bindParam(':date', $date_time);
+        $stmt->bindParam(':price', $price);
+        $stmt->bindParam(':desc', $description);
+        $stmt->bindParam(':id', $id);
+        if($stmt->execute()) {
+            return true;
+        }
+        return false;
     }
 
     public function insert_order($fio, $phone, $concert_id, $tickets, $status) {
--- app/views/admin/edit_concert.php
+++ app/views/admin/edit_concert.php
@@ -1,8 +1,16 @@
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+<form action="/admin/concerts/update" method="post">
+    <label>
+        <span>Date</span>
+        <input type="datetime-local" name="date" value="<?= date_format(date_create($this->concert['date']), 'Y-m-d\TH:m') ?>" required>
+    </label>
+    <label>
+        <span>Price</span>
+        <input type="text" name="price" value="<?= $this->concert['price'] ?>" required>
+    </label>
+    <label>
+        <span>Description</span>
+        <textarea name="description"><?= $this->concert['description'] ?></textarea>
+    </label>
+    <input type="hidden" name="id" value="<?= $this->concert['id'] ?>">
+    <input type="submit" name="send" value="Save">
+</form>
