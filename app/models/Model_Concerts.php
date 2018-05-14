d2ad6ad1dbf8fa16247331463c92a74b6a6802da
vadim <vadim@mbp-vadim>
2018-05-14 23:19:40 +0300

    redact Model_Concerts

--- app/models/Model_Concerts.php
+++ app/models/Model_Concerts.php
@@ -37,46 +37,21 @@ class Model_Concerts extends Model {
     }
 
     public function insert_concert($image, $date_time, $price, $description) {
-	$query = 'insert into '.$this->concerts_table.' (image, date, price, description) values(:image, :date,:price,:desc)';
-        $stmt = $this->db->prepare($query);
-        $stmt->bindParam(':image', $image);
-        $stmt->bindParam(':date', $date_time);
-        $stmt->bindParam(':price', $price);
-        $stmt->bindParam(':desc', $description);
-        if($stmt->execute()) {
-            return true;
-        }
-        return false;
+        
     }
 
     public function delete_concert($id) {
-	$query = 'delete from '.$this->concerts_table.' where id='.$id;
-        $stmt = $this->db->prepare($query);
-        if($stmt->execute()) {
-            return true;
-        }
-        return false;
+	
     }
 
     public function update_concert($id, $date_time, $price, $description) {
-	$query = 'update '.$this->concerts_table.' set date=:date, price=:price, description=:desc where id=:id';
-        $stmt = $this->db->prepare($query);
-        $stmt->bindParam(':date', $date_time);
-        $stmt->bindParam(':price', $price);
-        $stmt->bindParam(':desc', $description);
-        $stmt->bindParam(':id', $id);
-        if($stmt->execute()) {
-            return true;
-        }
-        return false;
+	
     }
 
     public function insert_order($name, $phone, $concert_id, $count_of_tickets) {
 	$query = "insert into ".$this->orders_table." (name, phone, concert_id, count_of_tickets)"
                 . " values('".$name."', '".$phone."', '".$concert_id."','".$count_of_tickets."');";
-        //var_dump($query);
         $queryResult = $this->dbConnection->query($query);
-        var_dump($query);
 	if ($queryResult) {
 	    return true;
 	}
