e90c2f0ba561663c323ec896dde508ef9e5e8343
alex3fokin <alex3.fokin@gmail.com>
2018-05-14 12:37:01 +0300

    deleted duplicated bootstrap php

--- app/views/bootstrap.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once 'app/core/Model.php';
-require_once 'app/core/View.php';
-require_once 'app/core/Controller.php';
-require_once 'app/core/Route.php';
-Route::start(); // запускаем маршрутизатор
-
