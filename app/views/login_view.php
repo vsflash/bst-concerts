a3351ad28175cf42a775b2748d31e7131a671d16
IhorSuprun <sup.on.php@gmail.com>
2018-05-15 16:21:03 +0300

    create some style in authorization.php, login_view.php, register_view.php

--- app/views/login_view.php
+++ app/views/login_view.php
@@ -1,28 +1,29 @@
 <!--TODO create style-->
-<div class="frm" style="width: 400px; margin: 20px">
-    <h2>Sign in to your account</h2>
-    <form method="post" action="/auth/signin" role="form" class="form-horizontal">
-        <div class="form-group">
-            <label for="login" class="control-label col-sm-2">Login</label>	
-            <div class="col-sm-10">
-                <input type="text" name="login" placeholder="login" class="form-control"/>
+<div class="form-wrapper">
+    <div class="frm" >
+        <h2>Sign in to your account</h2>
+        <form method="post" action="/auth/signin" role="form" class="form-horizontal">
+            <div class="form-group">
+                <label for="login" class="control-label col-sm-2">Login</label>	
+                <div class="col-sm-10">
+                    <input type="text" name="login" placeholder="Your login" class="form-control"/>
+                </div>
             </div>
-        </div>
             <div class="form-group">
                 <label for="pass" class="control-label col-sm-2">Password</label>	
                 <div class="col-sm-10">
-                    <input type="password" name="pass" placeholder="password" class="form-control"/>
+                    <input type="password" name="pass" placeholder="Your password" class="form-control"/>
                 </div>
             </div>
             <div class="form-group">
-                <div class="col-sm-offset-2 col-sm-10">
-                    <input type="submit" value="Sign in" class="btn btn-success">
+                <div class="col-sm-offset-1 col-sm-10">
+                    <input type="submit" value="Sign in" class="btn btn-success btn_auth">
                 </div>
             </div>
-    </form>
+        </form>
+    </div>
+    <a href="/auth/register"><button class="btn btn-primary btn_auth">Registration</button></a>
 </div>
-<a href="/auth/register"><button class="btn btn-primary">Registration</button></a>
-
 <!--    <h2>Sign in to your account</h2>
     <form method="post" action="/auth/signin">
       <input type="text" name="login" placeholder="login"/>
