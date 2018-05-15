a4e40b4a54090babba6bee1110b5b2b1aea8fae7
IhorSuprun <sup.on.php@gmail.com>
2018-05-15 11:55:33 +0300

    create style.css place link in authorization.php

--- app/views/templates/authorization.php
+++ app/views/templates/authorization.php
@@ -6,6 +6,7 @@
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css"> 
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script> 
+        <link href="../../../css/style.css" rel="stylesheet" type="text/css"/>
     </head>
     <body>
         <section>
